903510874f2d3f502fed384448c50800621ba290
Akeka_ <vladvaledol@gmail.com>
2025-05-29 15:58:43 +0300

    [lab 7]

--- lab7/main.cpp
+++ lab7/main.cpp
@@ -1,45 +1,60 @@
-#include <iostream>
 #include <iomanip>
+#include <iostream>
+#include <random>
 #include <stdio.h>
 #include <time.h>
-#include <random>
 using namespace std;
 
-const int C = 5;
-
-/// Генерация матрицы
-void inparr(int M[4][5]) {
-	cout << "Задан массив: \n";
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 5; j++) {
-			M[i][j] = -10 + rand() % 21;
-			cout << setw(4) << M[i][j];
-		}	
-		cout << "\n";
-	}
-	
+// Генерация матрицы
+void inparr(int **matr, int rows, int cols) {
+  cout << "Заполненная матрица " << rows << "x" << cols << ":\n";
+  for (int i = 0; i < rows; ++i) {
+    for (int j = 0; j < cols; ++j) {
+      matr[i][j] = -10 + rand() % 21; // числа от -10 до 10
+      cout << setw(4) << matr[i][j];
+    }
+    cout << "\n";
+  }
 }
 
-/// Умножение матрицы
-void processparr(int M[4][5]) {
-	cout << "\n\n";
-	int D[4][5];
-	cout << "Массив, умноженный на константу (" << C << "): \n";
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 5; j++) {
-			D[i][j] = M[i][j] * C;
-			cout << setw(4) << D[i][j];
-		}
-		cout << "\n";
-	}
-	cout << "\n\n";
+// Умножение матрицы
+void processparr(int **matr, int rows, int cols, int C) {
+  cout << "\nМатрица после умножения на " << C << ":\n";
+  for (int i = 0; i < rows; ++i) {
+    for (int j = 0; j < cols; ++j) {
+      cout << setw(4) << matr[i][j] * C;
+    }
+    cout << "\n";
+  }
 }
 
-/// Главная функция
+// Отчистка
+void freematr(int **matr, int rows) {
+  for (int i = 0; i < rows; ++i) {
+    delete[] matr[i];
+  }
+  delete[] matr;
+}
+
+// Главная функция
 int main() {
-    srand(time(NULL));
-	setlocale(0, "");
-	int D[4][5];
-	inparr(D);
-	processparr(D);
+  srand(time(NULL));
+  setlocale(0, "");
+  int rows, cols;
+
+  cout << "Введите строки --> ";
+  cin >> rows;
+  cout << "Введите столбцы --> ";
+  cin >> cols;
+
+  int **matr = new int *[rows];
+  for (int i = 0; i < rows; ++i) {
+    matr[i] = new int[cols];
+  }
+
+  inparr(matr, rows, cols);
+  processparr(matr, rows, cols, 5);
+  freematr(matr, rows);
+
+  return 0;
 }
